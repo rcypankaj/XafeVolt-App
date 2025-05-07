204917c4ed4a06a3b24d046da536a13271953010
Pankaj <pankaj.y@rejolut.com>
2025-05-08 05:15:29 +0530

    feat(change package json): change package json
    change package json

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
-
-/** @type {import('expo/metro-config').MetroConfig} */
+// Get default Expo config
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+// Add SVG support
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
+config.resolver.sourceExts.push("svg");
 
+// Apply NativeWind config
+module.exports = withNativeWind(config, { input: "./global.css" });
