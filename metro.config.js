b79cf33c6f4fd01948a316751da189a75e347d61
Pankaj <pankaj.y@rejolut.com>
2025-05-11 18:19:12 +0530

    feat(s): s
    a

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,17 @@
-/* eslint-env node */
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('@expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-// Get default Expo config
 const config = getDefaultConfig(__dirname);
 
-// Add SVG support
-config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
-config.resolver.sourceExts.push("svg");
+// Apply NativeWind config (modifies in place)
+withNativeWind(config, { input: './global.css' });
 
-// Apply NativeWind config
-module.exports = withNativeWind(config, { input: "./global.css" });
+// Add SVG support
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts.push('svg');
+
+// ✅ Add assetPlugins required for EAS build to work correctly
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
+module.exports = config;
